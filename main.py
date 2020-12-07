f700fc3889c690e9aa50ac66db0aab57a484944b
dedelvira98 <dedelvira98@gmail.com>
2020-12-07 19:11:33 +0700

    next

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('hello world')
+import random
+min = 1
+max = 6
+
+def roll():
+    roll_again = "yes"
+    while roll_again == "yes":
+        print ("Roll the dice...")
+        print ("The Values are")
+        print (random.randint(min, max))
+        roll_again = "no"
+
+roll()
+again = input("Do you still want to throw the dice?")
+
+if again == "yes":
+    roll()
+else:
+    print("Thank You, See You Again!")
+
+again = input("Do you still want to throw the dice?")
+
+if again == "yes":
+    roll()
+else:
+    print("Thank You, See You Again!")
